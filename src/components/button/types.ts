47c930e1cec7c9f309db0159871a9f67c4c1db9b
Marcell Kecskemety <mkecskemety@wellapp.com>
2022-11-13 15:46:17 +0100

    fixed format

--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,11 +1,11 @@
 export enum ButtonKind {
-  Primary = 'primary',
-  Featured = 'featured',
-  Text = 'text'
+	Primary = 'primary',
+	Featured = 'featured',
+	Text = 'text'
 }
 
 export enum ButtonSize {
-  Small = 'small',
-  Field = 'field',
-  Large = 'large'
+	Small = 'small',
+	Field = 'field',
+	Large = 'large'
 }
