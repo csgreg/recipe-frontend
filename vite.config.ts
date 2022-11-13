5c5148d044e5a6ba9f448f8077bc9e7dc7e03953
Gergo Csizmadia <61988487+csgreg@users.noreply.github.com>
2022-11-13 14:20:17 +0100

    setup style things (#14)
    * setup style things
    
    * format
    
    * add files
    
    Co-authored-by: Csizmadia Gergő <csgergo@Csizmadia-MacBook-Air.local>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import path from 'path';
+import { resolve } from 'path';
 
 const config: UserConfig = {
 	resolve: {
 		alias: {
-			// these are the aliases and paths to them
-			'@components': path.resolve('./src/components')
+			components: resolve('./src/components'),
+			src: resolve('./src')
 		}
 	},
 	plugins: [sveltekit()]
