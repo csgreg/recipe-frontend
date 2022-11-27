bc694c46d4399ce59ebf3c4f89c86487f7412778
Gergo Csizmadia <gergo.csizmadia@lensa.com>
2022-11-27 16:36:43 +0100

    format

--- src/routes.ts
+++ src/routes.ts
@@ -1,1 +1,5 @@
-export const api_routes = { create_recipe: '/api/recipes/create', signup: '/api/auth/signup', signin: '/api/auth/signin'};
+export const api_routes = {
+	create_recipe: '/api/recipes/create',
+	signup: '/api/auth/signup',
+	signin: '/api/auth/signin'
+};
